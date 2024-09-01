7f98a533c6845afc5cb6bc79387bb636b0605876
Sparsh Jain <utkarshj531@gmail.com>
2024-09-01 20:11:33 +0530

    add env file

--- src/components/ImageGenerator/ImageGenerator.jsx
+++ src/components/ImageGenerator/ImageGenerator.jsx
@@ -8,54 +8,57 @@ import axios from 'axios'; // Axios for making HTTP requests
 
 const ImageGenerator = () => {
 
-    const [image_url, setImage_url] = useState("/");
-    let inputRef = useRef(null);
-    const [loading,setLoading]=useState(false)
+  const [image_url, setImage_url] = useState("/");
+  let inputRef = useRef(null);
+  const [loading, setLoading] = useState(false)
 
-    const ImageGenerator = async () => {
-        setLoading(true); // Set loading to true when the function starts
-        const options = {
-          method: 'POST', // HTTP method
-          url: 'https://ai-image-generator3.p.rapidapi.com/generate', // API endpoint
-          headers: {
-            'x-rapidapi-key': '092755a4acmsh6cb353d5f48521fp1f98f0jsn5f8c01f416cd', // API key
-            'x-rapidapi-host': 'ai-image-generator3.p.rapidapi.com', // API host
-            'Content-Type': 'application/json' // Content type of the request
-          },
-          data: {
-            prompt: inputRef.current.value, // Data payload with the input text
-            page: 1 // Page number for the API request
-          }
-        };
-    
-        try {
-          const response = await axios.request(options); // Await the API response
-          console.log('API Response:', response); // Log the API response
-          setImage_url(response.data.results.images[0]); // Set the images state with the API response 
-        } catch (error) {
-          console.error('Error generating image:', error); // Log any errors
-        } finally {
-          setLoading(false); // Set loading to false when the function ends
-        }
-      };
+  const apiKey = import.meta.env.VITE_API_KEY;
 
-    return (
-        <div className='ai-image-generator'>
-            <div className="header">Ai Image <span>generator</span></div>
-            <div className="img-loading">
-                <div className="image"><img src={image_url === "/" ? default_img : image_url} alt="" />
-                <div className="loading">
-                    <div className={loading?"loading-bar-full":"loading-bar"}></div>
-                    <div className={loading?"loading-text":"display-none"}>Loadinng...</div>
-                </div>
-                </div>
-            </div>
-            <div className="search-box">
-                <input type="text" ref={inputRef} className='search-input' placeholder='Describe what you want to see' />
-                <div className="generate-btn" onClick={() => { ImageGenerator() }}>Generate</div>
-            </div>
+
+  const ImageGenerator = async () => {
+    setLoading(true); // Set loading to true when the function starts
+    const options = {
+      method: 'POST', // HTTP method
+      url: 'https://ai-image-generator3.p.rapidapi.com/generate', // API endpoint
+      headers: {
+        'x-rapidapi-key': `${apiKey}`, // API key
+        'x-rapidapi-host': 'ai-image-generator3.p.rapidapi.com', // API host
+        'Content-Type': 'application/json' // Content type of the request
+      },
+      data: {
+        prompt: inputRef.current.value, // Data payload with the input text
+        page: 1 // Page number for the API request
+      }
+    };
+
+    try {
+      const response = await axios.request(options); // Await the API response
+      console.log('API Response:', response); // Log the API response
+      setImage_url(response.data.results.images[0]); // Set the images state with the API response 
+    } catch (error) {
+      console.error('Error generating image:', error); // Log any errors
+    } finally {
+      setLoading(false); // Set loading to false when the function ends
+    }
+  };
+
+  return (
+    <div className='ai-image-generator'>
+      <div className="header">Ai Image <span>generator</span></div>
+      <div className="img-loading">
+        <div className="image"><img src={image_url === "/" ? default_img : image_url} alt="" />
+          <div className="loading">
+            <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
+            <div className={loading ? "loading-text" : "display-none"}>Loadinng...</div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="search-box">
+        <input type="text" ref={inputRef} className='search-input' placeholder='Describe what you want to see' />
+        <div className="generate-btn" onClick={() => { ImageGenerator() }}>Generate</div>
+      </div>
+    </div>
+  )
 }
 
 export default ImageGenerator
